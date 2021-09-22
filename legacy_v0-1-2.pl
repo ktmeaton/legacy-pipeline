be3a9a869bf30a0d4d1dda841bf9cee0a356d8cb
Katherine Eaton <ktmeaton@gmail.com>
2021-09-22 15:59:08 -0400

    finish testing for v0.2.1

--- legacy_v0-1-2.pl
+++ legacy_v0-1-2.pl
@@ -1,32 +1,54 @@
 #!/usr/bin/perl
 
-## Poinar Lab updated sequencing pipeline
+## Poinar Lab sequencing pipeline for ANCIENT samples
 ## Ana Duggan's parting gift 
-## Last modifed Aug252021
+## Last modifed Sept 22 2021
 
 #use strict;
 use warnings;
 use Data::Dumper;
-if (scalar(@ARGV) < 2) {
-	  print "\n";
-		print "Version : v0.1.2\n";
-		print "Date    : 2021-08-25\n";
-    print "Usage   : legacy.pl input_fastq_directory reference_genome(s)\n";
-		print "Notes   : Redirect output to file called 'Makefile' and launch with command 'make'\n";
-		print "          Reference genomes must be indexed using network aware bwa found at /home/ana/scripts/network-aware-bwa/bwa\n";
-		print "\n";
-		exit;
+
+# ------------------------------
+# Print Usage Information
+
+if (scalar(@ARGV) < 2) 
+{
+    print "\n";
+    print "Version : v0.1.2\n";
+    print "Date    : 2021-08-25\n";
+    print "Usage   : legacy.pl input_fastq_directory reference_genome(s) (modern)\n";
+    print "Notes   : Redirect output to file called 'Makefile' and launch with command 'make'\n";
+    print "          Reference genomes must be indexed using network aware bwa found at /home/ana/scripts/network-aware-bwa/bwa\n";
+    print "          If the final parameter is 'modern' this will disable --ancientdna mode for leeHom.\n";
+    print "\n";
+    exit;
 };
+
+
+# ------------------------------
+# Variable Declaration
 
 my $directory = $ARGV[0];
-
 my $ref = $ARGV[1];
-
 my @refgenomes;
-
-for (my $i=2; $i<scalar(@ARGV); $i++) {
+for (my $i=2; $i<scalar(@ARGV); $i++) 
+{
     push(@refgenomes, $ARGV[$i]);
 };
+
+
+# The last argument will be checked for the keyword 'modern'
+my $modern_flag = $ARGV[$#ARGV];
+my $ancient_flag = "";
+if($modern_flag ne "modern")
+{
+    $ancient_flag = "--ancientdna";
+}
+else
+{
+    # remove the word "modern" from the list of reference genomes
+    pop @refgenomes;
+}
 
 my $fq2bam = "/home/ana/scripts/BCL2BAM2FASTQ/fastq2bam/fastq2bam";
 my $bwa    = "/home/ana/scripts/network-aware-bwa/bwa";
@@ -45,29 +67,55 @@ my @sampleArray = readDirSample($directory);
 
 #print Dumper(@sampleArray);
 
-for my $directorySample (@sampleArray){
+for my $directorySample (@sampleArray)
+{
     my @mapped;
     my %extramapped;
-    my @fieldSample=split("_",$directorySample);   ## Splits directory name into elements on underscores
-#    print "my directory sample: $directorySample\nmy filed sapmple: Dumper(@fieldSample)\n";
-    if($#fieldSample <  1){  ## Ensures there is more than one element or dies
+    
+    # Split directory name into elements on underscores
+    my @fieldSample=split("_",$directorySample);
+
+    # print "my directory sample: $directorySample\nmy filed sapmple: Dumper(@fieldSample)\n";
+    ## Ensures there is more than one element or dies
+    if($#fieldSample <  1)
+    {  
 	die "Unexpected number of fields for filename $directorySample, found ".$#fieldSample." fields ... exiting\n";
     }
-    my $sampleName= $fieldSample[$#fieldSample];   ## Assigns final element of directory name as sample identifier
-    my @gzArray = readDirFile($directorySample);   ## Reads through file names of directory, pushes them into an array
 
-    for my $gzFile (@gzArray){
-	my @fieldGZ=split("_",$gzFile);   ## Split file names on underscores
-	if($#fieldGZ <  4){
-	    die "Unexpected number of fields for file $gzFile ... exiting\n";  ## Dies if there aren't the expected 5 fields (Sample name, index pair, Lane, Read, file #)
+    # Assigns final element of directory name as sample identifier
+    my $sampleName= $fieldSample[$#fieldSample];
+
+    # Reads through file names of directory, pushes them into an array
+    my @gzArray = readDirFile($directorySample);
+
+    # --------------------------------------------------
+    # Create command to align each file to the reference
+    
+    for my $gzFile (@gzArray)
+    {
+        # Split file names on underscores
+	my @fieldGZ=split("_",$gzFile);
+
+	# Dies if there aren't the expected 5 fields (Sample name, index pair, Lane, Read, file #)
+	if($#fieldGZ <  4)
+        {
+	    die "Unexpected number of fields for file $gzFile ... exiting\n";
 	}	
-	if($fieldGZ[$#fieldGZ-1] eq "R1"){  ## Check if R1 file exists
+        # Check if R1 file exists
+	if($fieldGZ[$#fieldGZ-1] eq "R1")
+        {
             my @fieldsToJoin = @fieldGZ;   
-	    $fieldsToJoin[ $#fieldGZ-1 ] = "R2";  ## Knowing the R1 file exists, swap R1 for R2 in name only
-            
-	    my $filer2 =  join("_",@fieldsToJoin); ##join together fields
+
+            # Swap R1 for R2 in name only
+	    $fieldsToJoin[ $#fieldGZ-1 ] = "R2";
+
+            # Join fields together
+	    my $filer2 =  join("_",@fieldsToJoin);
+
             # If the R2 file doesn't exist (ie. single-end) just make the file name blank
-            if ( ! -e $filer2 ){
+            # Make a note in the Makefile that this sample is single-end
+            if ( ! -e $filer2 )
+            {
 		print "# Single end sample: $gzFile\n"; 
              	$filer2 = "";
             }
@@ -77,11 +125,18 @@ for my $directorySample (@sampleArray){
             my @fullRefName = split("\\.", $fieldREF[-1]);
 	    my $refName = $fullRefName[0];
 	    my $tempOutFile = $gzFile.$refName.".sort.bam";
-	    my $cmd =  "$fq2bam -r  $sampleName -o /dev/stdout ".$gzFile."\t".$filer2."  | leeHom --ancientdna --log ".$gzFile."leehom.txt"." -o /dev/stdout  /dev/stdin | $bwa bam2bam -n 0.01 -o 2 -l 16500 -g $fullRefPath /dev/stdin | $samtools sort -o /dev/stdin ".$gzFile.".sort"." >$tempOutFile";  ## Converts 2 paired fastq files to bam
+
+            # Convert fastq files to bam format, align to reference genome, sort
+	    my $cmd =  "$fq2bam -r  $sampleName -o /dev/stdout ".$gzFile."\t".$filer2."  | leeHom $ancient_flag --log ".$gzFile."leehom.txt"." -o /dev/stdout  /dev/stdin | $bwa bam2bam -n 0.01 -o 2 -l 16500 -g $fullRefPath /dev/stdin | $samtools sort -o /dev/stdin ".$gzFile.".sort"." >$tempOutFile";
 	    push(@listTargets,  $tempOutFile);
 	    push(@mapped,  $tempOutFile);
-	    $stringMakeToPrint = $stringMakeToPrint.$tempOutFile.":\n\t$cmd\n\n";    ## Add command to vairable which will be printed in Makefile
-	    for (my $i=0; $i<scalar(@refgenomes); $i++) {
+
+	    # Add command to variable which will be printed in Makefile
+	    $stringMakeToPrint = $stringMakeToPrint.$tempOutFile.":\n\t$cmd\n\n";
+
+            # Repeat the command creation process for (optional) additional references
+	    for (my $i=0; $i<scalar(@refgenomes); $i++) 
+            {
 	        my $extraRefPath = $refgenomes[$i];
 	        my @extraFieldRef = split("/", $extraRefPath);
 	        my @extraFullRefName = split("\\.", $extraFieldRef[-1]);
@@ -93,15 +148,22 @@ for my $directorySample (@sampleArray){
     	        $stringMakeToPrint = $stringMakeToPrint.$extraTempOutFile.": ".$tempOutFile."\n\t$extraCmd\n\n";
 	    }
 	}
-    else
-    {
-	    if($fieldGZ[ $#fieldGZ-1 ] eq "R2"){
-		#ignore
-	    }else{
-		die "Unexpected 5th field for file $gzFile ... exiting\n";
-	    }
-	}
+        else
+        {
+            if($fieldGZ[ $#fieldGZ-1 ] eq "R2")
+            {
+	        #ignore
+            }
+            else
+            {
+	        die "Unexpected 5th field for file $gzFile ... exiting\n";
+            }
+        }
     }
+
+    # ----------------------------------------------------------
+    # Create commands to merge and post-process the aligned files
+    
     my $fullRefPath =  $ref;
     my @fieldREF = split("/", $fullRefPath);
     my @fullRefName = split("\\.", $fieldREF[-1]);
@@ -116,56 +178,75 @@ for my $directorySample (@sampleArray){
     my $exhaustionFileName = $sampleName."_".$refName.".Exhaust.txt";
     my $fileList = join(" ", @mapped);
     
-    my $metaCmd = "$samtools merge $fileList | $samtools view -m 24 /dev/stdin | cut -f 1,10 | agrep -v -1 AGATCGGAA | agrep -v -1 TTCCGATCT | sed 's/^/>/g' | sed 's/\\t/\\n/g'  >$metaReadsFileName"; ##Combine the multiple bam files for a single library, apply 24 bp length filter and use agrep to filter for remaining similarity to adapter sequences
+    # Output for metagenomics
+    #   1. Combine the multiple bam files for a single library, 
+    #   2. Apply 24 bp length filter,
+    #   3/ Use agrep to filter for remaining similarity to adapter sequences
+    my $metaCmd = "$samtools merge $fileList | $samtools view -m 24 /dev/stdin | cut -f 1,10 | agrep -v -1 AGATCGGAA | agrep -v -1 TTCCGATCT | sed 's/^/>/g' | sed 's/\\t/\\n/g'  >$metaReadsFileName";
     push(@listTargets,$metaReadsFileName);
     $stringMakeToPrint = $stringMakeToPrint.$metaReadsFileName.": $fileList\n\t$metaCmd\n\n";
-    my $uniqCmd = "$dedup --fasta $metaReadsFileName --output $uniqMetaReadsFileName --stats $dedupStatsFileName"; ##String deduplicate metagenomic reads
+
+    # Remove duplicates based on string deduplication
+    my $uniqCmd = "$dedup --fasta $metaReadsFileName --output $uniqMetaReadsFileName --stats $dedupStatsFileName";
     push(@listTargets,$uniqMetaReadsFileName);
     $stringMakeToPrint = $stringMakeToPrint.$uniqMetaReadsFileName.": $metaReadsFileName\n\t$uniqCmd\n\n";
+
+    # Combine the multiple bam files for a single library 
+    # (derived from the multiple fastq files where libraries are split across lanes 
+    # and multiple files due to line limits) 
+    # into a single bam file for a given library and reference combination
     my $mmfCmd = "$samtools merge $fileList | $retrieve /dev/stdin /dev/stdout | $rmdup -c -o $mapFileName /dev/stdin 1>$exhaustionFileName";  ## Combine the multiple bam files for a single library (derived from teh multiple fastq files where libraries are split across lanes and multiple files due to line limits) into a single bam file for a given library and reference combination
     push(@listTargets,$mapFileName);
     $stringMakeToPrint = $stringMakeToPrint.$mapFileName.": $fileList\n\t$mmfCmd\n\n";
+
+    # Calculate the fragment length distribution
     my $fldCmd = "$fld $mapFileName | sort >$fldFileName ";
     push(@listTargets, $fldFileName);
     $stringMakeToPrint = $stringMakeToPrint.$fldFileName.": $mapFileName\n\t$fldCmd\n\n";
+
+    # Filter for length and map quality
     my $minCmd = "$samtools view -b -m 24 -q 30 -o $filterFileName $mapFileName";
     push(@listTargets, $filterFileName);
     $stringMakeToPrint = $stringMakeToPrint.$filterFileName.": $mapFileName\n\t$minCmd\n\n";
-    
-    
-    for (my $i=0; $i<scalar(@refgenomes); $i++) {
-			my $extraRefPath = $refgenomes[$i];
-			my @extraFieldRef = split("/", $extraRefPath);
-			my @extraFullRefName = split("\\.", $extraFieldRef[-1]);
-			my $extraRefName = $extraFullRefName[0];
-			my $extraMapFileName = $sampleName."_".$extraRefName.".mapped.bam";
-			my $extraFLDFileName = $sampleName."_".$extraRefName.".FLD.txt";
-			my $extraPlotFileName = $sampleName."_".$extraRefName.".FLD.pdf";
-			my $extraFilterFileName = $sampleName."_".$extraRefName.".min24MQ30.bam";
-			my $extraFileList = join(" ", @{$extramapped{$extraRefName}});
+   
+    # Repeat the metagenomics prep commands for (optional) additional references 
+    for (my $i=0; $i<scalar(@refgenomes); $i++) 
+    {
+        my $extraRefPath = $refgenomes[$i];
+	my @extraFieldRef = split("/", $extraRefPath);
+	my @extraFullRefName = split("\\.", $extraFieldRef[-1]);
+	my $extraRefName = $extraFullRefName[0];
+        my $extraMapFileName = $sampleName."_".$extraRefName.".mapped.bam";
+	my $extraFLDFileName = $sampleName."_".$extraRefName.".FLD.txt";
+	my $extraPlotFileName = $sampleName."_".$extraRefName.".FLD.pdf";
+	my $extraFilterFileName = $sampleName."_".$extraRefName.".min24MQ30.bam";
+	my $extraFileList = join(" ", @{$extramapped{$extraRefName}});
     	my $extraExhaustionFileName = $sampleName."_".$extraRefName.".Exhaust.txt";
 	
-			my $extraMmfCmd = "$samtools merge $extraFileList | $retrieve /dev/stdin /dev/stdout | $rmdup -c -o $extraMapFileName /dev/stdin 1>$extraExhaustionFileName";
-			push(@listTargets, $extraMapFileName);
-			$stringMakeToPrint = $stringMakeToPrint.$extraMapFileName.": $extraFileList\n\t$extraMmfCmd\n\n";
-			my $extraFldCmd = "$fld $extraMapFileName | sort >$extraFLDFileName ";
-			push(@listTargets, $extraFLDFileName);
-			$stringMakeToPrint = $stringMakeToPrint.$extraFLDFileName.": $extraMapFileName\n\t$extraFldCmd\n\n";
-			my $extraMinCmd = "$samtools view -b -m 24 -q 30 -o $extraFilterFileName $extraMapFileName";
-			push(@listTargets, $extraFilterFileName);
-			$stringMakeToPrint = $stringMakeToPrint.$extraFilterFileName.": $extraMapFileName\n\t$extraMinCmd\n\n";
+        my $extraMmfCmd = "$samtools merge $extraFileList | $retrieve /dev/stdin /dev/stdout | $rmdup -c -o $extraMapFileName /dev/stdin 1>$extraExhaustionFileName";
+	push(@listTargets, $extraMapFileName);
+        $stringMakeToPrint = $stringMakeToPrint.$extraMapFileName.": $extraFileList\n\t$extraMmfCmd\n\n";
+	my $extraFldCmd = "$fld $extraMapFileName | sort >$extraFLDFileName ";
+        push(@listTargets, $extraFLDFileName);
+	$stringMakeToPrint = $stringMakeToPrint.$extraFLDFileName.": $extraMapFileName\n\t$extraFldCmd\n\n";
+	my $extraMinCmd = "$samtools view -b -m 24 -q 30 -o $extraFilterFileName $extraMapFileName";
+	push(@listTargets, $extraFilterFileName);
+	$stringMakeToPrint = $stringMakeToPrint.$extraFilterFileName.": $extraMapFileName\n\t$extraMinCmd\n\n";
     }
 }
 
-
+# --------------------------------------
+# Write all commands to the Makefile
 print "\nSHELL := /bin/bash\n\nDefault:\tall\n\n".$stringMakeToPrint."\n\nall:\t".join(" ",@listTargets)."\n\nclean:\n\trm -fv ".join(" ",@listTargets)."\n\n";
 
 
 
-####Subroutines####
+# ---------------------------------
+# Subroutines
 
 #Reads in list of directory names from stdin, 
-sub readDirSample{
+sub readDirSample
+{
     my ($dir) =  shift;
     my @arrayToReturn;
     opendir(D, $dir) || die "Can't open directory: $dir\n";
@@ -180,7 +261,8 @@ sub readDirSample{
     return @arrayToReturn;
 }
 
-sub readDirFile{
+sub readDirFile
+{
     my ($dir) =  shift;
     my @arrayToReturn;
     opendir(D, $dir) || die "Can't open directory: $dir\n";
